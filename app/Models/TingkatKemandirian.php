92c66faa4ff9968a737a515189e2340ce73b0a27
dendik-creation <setiawandendik0205@gmail.com>
2024-08-25 22:21:32 +0700

    fix: fix cast to casts

--- app/Models/TingkatKemandirian.php
+++ app/Models/TingkatKemandirian.php
@@ -10,7 +10,7 @@ class TingkatKemandirian extends Model
     use HasFactory;
     protected $guarded = ['id'];
     protected $hidden = ['created_at', 'updated_at'];
-    protected $cast = [
+    protected $casts = [
         'step' => 'integer',
     ];
 }
