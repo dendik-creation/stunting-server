7752a3034bfca8ef46bff8f7e54c2fa878cfed32
dendik-creation <setiawandendik0205@gmail.com>
2024-09-04 22:40:07 +0700

    fix: Fix redirect index page to login page

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/dashboard/login');
 });
 
 Route::prefix('keluarga')->middleware('auth')->group(function(){
